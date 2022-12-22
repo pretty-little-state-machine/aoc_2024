f3db503f1a4667d118f429b7742d6b44e71d64ab
Zoey <zoey.fahner@gmail.com>
2022-12-22 11:02:55 -0500

    P2 finished.

--- src/bin/21.rs
+++ src/bin/21.rs
@@ -1,14 +1,17 @@
+#![feature(let_chains)]
+
+use crate::MonkeyAction::{Add, Divide, Multiply, Subtract, Yell};
 use rustc_hash::FxHashMap;
-use crate::MonkeyAction::{Yell, Add, Divide, Multiply, Subtract};
 
 type Monkeys = FxHashMap<String, Monkey>;
 
 #[derive(Clone, Default, Debug)]
 struct Monkey {
     action: MonkeyAction,
-    value: usize,
-    left: Option<String>,
-    right: Option<String>,
+    original_action: MonkeyAction,
+    value: isize,
+    left: String,
+    right: String,
 }
 
 #[derive(Clone, Default, Debug, Eq, PartialEq)]
@@ -24,39 +27,30 @@ enum MonkeyAction {
 fn build_monkeys(input: &str, monkeys: &mut Monkeys) {
     let name = input[0..4].to_string();
     let mut monkey = Monkey::default();
-    if input.len() > 10 {
-        monkey.right = Some(input[6..10].to_string());
-        monkey.left = Some(input[13..17].to_string());
+    if input.len() == 17 {
+        monkey.right = input[6..10].to_string();
+        monkey.left = input[13..17].to_string();
         monkey.action = match input.chars().nth(11).unwrap() {
             '+' => Add,
             '/' => Divide,
             '*' => Multiply,
             '-' => Subtract,
-            _ => unreachable!("Invalid MonkeyAction Operation")
+            _ => unreachable!("Invalid MonkeyAction Operation"),
         }
     } else {
         monkey.action = Yell;
-        monkey.value = input[6..].parse::<usize>().unwrap();
+        monkey.value = input[6..].parse::<isize>().unwrap();
     };
+    monkey.original_action = monkey.action.clone();
     monkeys.insert(name, monkey);
 }
 
-pub fn part_one(input: &str) -> Option<usize> {
-    let mut monkeys = FxHashMap::default();
-    input.lines().for_each(|l| build_monkeys(l, &mut monkeys));
-
-
-    let mut stack: Vec<String> = Vec::with_capacity(monkeys.len());
-    stack.push("root".to_string());
-    while stack.len() > 0 {
-        let monkey = monkeys.get(&*stack.pop().unwrap()).unwrap();
-        println!("{:?}", monkey);
-    }
-
-    /*
+/// Runs the monkey yelling simulation and returns a hashmap of parents for Part II use.
+fn resolve_monkey_shouts(monkeys: &mut Monkeys) -> FxHashMap<String, String> {
     let key_map = monkeys.clone();
     let keys = key_map.keys().collect::<Vec<&String>>();
-    // Horrific lol
+    let mut parents: FxHashMap<String, String> = FxHashMap::default();
+
     loop {
         if monkeys.get(&"root".to_string()).unwrap().action == Yell {
             break;
@@ -67,8 +61,17 @@ pub fn part_one(input: &str) -> Option<usize> {
             if m.action == Yell {
                 continue;
             }
+
             let a = monkeys.get(&*m.right).unwrap().clone();
             let b = monkeys.get(&*m.left).unwrap().clone();
+            parents.insert(
+                (*m.right).parse().unwrap(),
+                (**&(*key).clone()).parse().unwrap(),
+            );
+            parents.insert(
+                (*m.left).parse().unwrap(),
+                (**&(*key).clone()).parse().unwrap(),
+            );
             let m = monkeys.get_mut(*key).unwrap();
 
             if a.action == Yell && b.action == Yell {
@@ -83,63 +86,100 @@ pub fn part_one(input: &str) -> Option<usize> {
             }
         }
     }
-    Some(monkeys.get(&*"root".to_string()).unwrap().value)
-
-     */
-    Some(0)
+    parents
 }
 
-pub fn part_two(input: &str) -> Option<usize> {
-    /*
-     let mut original_monkeys = FxHashMap::default();
-     input.lines().for_each(|l| build_monkeys(l, &mut original_monkeys));
-     let key_map = original_monkeys.clone();
-     let keys = key_map.keys().collect::<Vec<&String>>();
+pub fn part_one(input: &str) -> Option<isize> {
+    let mut monkeys = FxHashMap::default();
+    input.lines().for_each(|l| build_monkeys(l, &mut monkeys));
+    resolve_monkey_shouts(&mut monkeys);
+    Some(monkeys.get(&*"root".to_string()).unwrap().value)
+}
 
-     // Even worse, but I really just want a win. Please advent, I'm beggin' ya
-     let mut number_to_yell = 0;
-     'num_gen: for test_number in 0..10_000_000 {
-         let mut monkeys = original_monkeys.clone();
-         number_to_yell = test_number;
-         monkeys.get_mut(&"humn".to_string()).unwrap().value = test_number;
-         loop {
-             // Actually the equality check, we can ignore the root value and compare children
-             let root = monkeys.get(&"root".to_string()).unwrap();
-             if root.action == Yell {
-                 let a = monkeys.get(root.children.first().unwrap()).unwrap().clone();
-                 let b = monkeys.get(root.children.get(1).unwrap()).unwrap().clone();
-                 if a.value == b.value {
-                     break 'num_gen;
-                 }
-                 break;
-             }
-             // Resolve as much as we can each pass
-             for key in &keys {
-                 let m = monkeys.get(*key).unwrap();
-                 if m.action == Yell {
-                     continue;
-                 }
-                 let a = monkeys.get(m.children.first().unwrap()).unwrap().clone();
-                 let b = monkeys.get(m.children.get(1).unwrap()).unwrap().clone();
-                 let m = monkeys.get_mut(*key).unwrap();
+pub fn part_two(input: &str) -> Option<isize> {
+    let mut monkeys = FxHashMap::default();
+    input.lines().for_each(|l| build_monkeys(l, &mut monkeys));
+    let parents = resolve_monkey_shouts(&mut monkeys);
 
-                 if a.action == Yell && b.action == Yell {
-                     match m.action {
-                         Yell => (),
-                         Add => m.value = a.value + b.value,
-                         Divide => m.value = a.value / b.value,
-                         Multiply => m.value = a.value * b.value,
-                         Subtract => m.value = a.value.saturating_sub(b.value),
-                     }
-                     m.action = Yell;
-                 }
-             }
-         }
-     }
-     Some(number_to_yell)
+    let mut current_child = "humn".to_string();
+    let mut path: Vec<String> = Vec::with_capacity(100);
+    // path.push(current_child.clone());
+    while let Some(parent) = parents.get(&current_child) {
+        path.push(parent.clone());
+        if parent == "root" {
+            break;
+        }
+        current_child = (*parent.clone()).parse().unwrap();
+    }
+    // Determine if we need to match left or right side of the overall tree
+    let root = monkeys.get(&"root".to_string()).unwrap();
+    let child_to_match = if root.left == current_child {
+        root.right.clone()
+    } else {
+        root.left.clone()
+    };
 
-     */
-    Some(0)
+    // Now search down and gather the path to HUMN
+    let value_to_match = monkeys.get(&child_to_match).unwrap().value;
+    let mut last_visited = "humn".to_string();
+    // Operations are the computation and a bool that is true if the operation is LHS
+    let mut ops: Vec<(MonkeyAction, bool)> = Vec::new();
+    let mut values = Vec::new();
+    for p in path {
+        if "root" == p {
+            continue;
+        }
+        let monkey = monkeys.get(&p).unwrap();
+        // Resolve the OPPOSITE side of the tree from the walk up from HUMN for our equation
+        if monkey.right == last_visited {
+            let monkey_val = monkey.left.clone();
+            let value = monkeys.get(&monkey_val).unwrap().value;
+            values.push(value);
+            match monkey.original_action {
+                Yell => (),
+                Add => ops.push((Add, true)),
+                Divide => ops.push((Divide, true)),
+                Multiply => ops.push((Multiply, true)),
+                Subtract => ops.push((Subtract, true)),
+            }
+        } else {
+            let monkey_val = monkey.right.clone();
+            let value = monkeys.get(&monkey_val).unwrap().value;
+            values.push(value);
+            match monkey.original_action {
+                Yell => (),
+                Add => ops.push((Add, false)),
+                Divide => ops.push((Divide, false)),
+                Multiply => ops.push((Multiply, false)),
+                Subtract => ops.push((Subtract, false)),
+            }
+        };
+        last_visited = p;
+    }
+
+    // Reverse the path and solve the linear equation
+    let mut x = value_to_match;
+    for (idx, (action, lhs)) in ops.iter().enumerate().rev() {
+        let v = *values.get(idx).unwrap();
+        if *lhs {
+            match action {
+                Yell => (),
+                Add => x -= v,
+                Divide => x *= v,
+                Multiply => x /= v,
+                Subtract => x += v,
+            }
+        } else {
+            match action {
+                Yell => (),
+                Add => x -= v,
+                Divide => x = v / x,
+                Multiply => x /= v,
+                Subtract => x = v - x,
+            }
+        }
+    }
+    Some(x)
 }
 
 fn main() {
