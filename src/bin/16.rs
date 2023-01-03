32bf77271041da6da902b71dc9d9716b91ea3a56
Zoey <zoey.fahner@gmail.com>
2023-01-02 19:57:47 -0500

    Day 16 - finally finished.

--- src/bin/16.rs
+++ src/bin/16.rs
@@ -1,191 +1,219 @@
-use petgraph::algo::{floyd_warshall, NegativeCycle};
-use petgraph::prelude::NodeIndex;
-use petgraph::{Directed, Graph};
 use regex::Regex;
 use rustc_hash::FxHashMap;
-use std::collections::HashMap;
-use std::thread::current;
-
-type Pipeline = FxHashMap<usize, Valve>;
+use std::cmp::{max, min};
 
 #[derive(Debug, Eq, Hash, Clone, PartialEq)]
 struct Valve {
-    id: usize,
-    node_index: Option<NodeIndex>,
+    id: String,
     flow_rate: usize,
-    neighbors: Vec<usize>,
-    is_open: bool,
+    neighbors: Vec<String>,
 }
 
-/// Converts the valve string into a unique usize reference so we avoid strings as keys
-#[inline(always)]
-fn valve_str_to_idx(input: &str) -> usize {
-    let chars = input.chars().collect::<Vec<char>>();
-    (*chars.get(0).unwrap() as usize * 1000) + (*chars.get(1).unwrap() as usize)
-}
-
+/// Parses the input string into a Valve struct containing a vector of neighboring nodes and flow
 impl Valve {
     pub fn new(input: &str) -> Self {
         let re = Regex::new(r".*([A-Z]{2}).*=(\d+).*[se]\s([A-Z,\s]+)").unwrap();
         let cap = re.captures(input).unwrap();
         Self {
-            id: valve_str_to_idx(&cap[1]),
-            node_index: None,
+            id: cap[1].to_string(),
             flow_rate: cap[2].parse::<usize>().unwrap(),
             neighbors: cap[3]
                 .split(", ")
-                .map(valve_str_to_idx)
-                .collect::<Vec<usize>>(),
-            is_open: false,
+                .map(|s| s.to_string())
+                .collect::<Vec<String>>(),
         }
     }
 }
 
-fn gml(pipeline: &Pipeline) {
-    println!("graph\n[");
-    for (k, _) in pipeline.iter() {
-        println!("node\n[\nid {}\n]\n", k)
-    }
-    for (k, v) in pipeline.iter() {
-        for e in &v.neighbors {
-            println!("edge\n[\nsource {}\ntarget {}\n]\n", k, e)
-        }
-    }
-    println!("]")
-}
+/// Builds up a few datastructures that will be used in the problem.
+///
+/// **valve_distances** Vec<Vec<usize>>
+///   A fully-meshed graph of valves where the value represents the time to move between valves. It
+///   has to include all valves since even valves with 0 flow rate contribute to movement distance.
+///
+/// **flow_rates** FxHashMap<String, usize> - Filtered to Flow Rate > 0
+///   A HashMap with the key of the valve String and the flow-rate as the value.
+///
+/// **valve_mask** FxHashMap<String, usize> - Filtered to Flow Rate > 0
+///   This HashMap has the valve String as the key and the value is a unique integer that maps to a
+///   bit. This ID will be used later in the path finding to easily store visited state with boolean
+///   math. Ex: AA = 001, BB = 010, CC = 100, DD = 1000, etc... thus the state value of 6 (1010)
+///   would mean that valves `AA` and `DD` were visited. Only valves with a flow rate > 0 are part
+///   this HashMap.
+///
+fn build_structures(
+    input: &str,
+) -> (
+    FxHashMap<String, FxHashMap<String, usize>>,
+    FxHashMap<String, usize>,
+    FxHashMap<String, usize>,
+) {
+    let mut flow_rates = FxHashMap::default();
 
-fn build_graph(input: &str) -> (Graph<usize, usize, Directed>, Pipeline) {
-    let mut graph: Graph<usize, usize, Directed> = Graph::default();
-    let mut valves = Pipeline::default();
-
-    for line in input.lines() {
-        let mut valve = Valve::new(line);
-        valve.node_index = Some(graph.add_node(valve.flow_rate));
-        valves.insert(valve.id, valve);
-    }
-
-    for (_, valve) in &valves {
-        for neighbor in &valve.neighbors {
-            graph.add_edge(
-                valve.node_index.unwrap(),
-                valves.get(&neighbor).unwrap().node_index.unwrap(),
-                1,
-            );
-        }
-    }
-
-    (graph, valves)
-}
-
-fn find_best_next(
-    current_valve_id: usize,
-    node_distances: &HashMap<(NodeIndex, NodeIndex), usize>,
-    pipeline: &Pipeline,
-) -> (Option<usize>, Option<usize>) {
-    let current_valve = pipeline.get(&current_valve_id).unwrap();
-    let mut best_flow_rate: usize = 0;
-    let mut best_next_valve_id: Option<usize> = None;
-    let mut best_node_distance: Option<usize> = None;
-    'outer: for step in 0..10_usize {
-        for (_, valve) in pipeline {
-            // We don't care about the current valve we are at or any already open valves
-            if current_valve.node_index.unwrap() == valve.node_index.unwrap() || valve.is_open {
-                continue;
+    // Decode the valve input text into a Vec<Valve> for easier datastructure construction later
+    let valves: Vec<Valve> = input
+        .lines()
+        .map(|line| {
+            let valve = Valve::new(line);
+            if valve.flow_rate > 0 {
+                flow_rates.insert(valve.id.clone(), valve.flow_rate);
             }
-            if let Some(distance) =
-            node_distances.get(&(current_valve.node_index.unwrap(), valve.node_index.unwrap()))
-            {
-                let flow_rate = (step.saturating_sub(*distance) * valve.flow_rate);
-                // println!("{} Estimated Flow Rate {}, {}", step, decode_valve_id(valve.id), flow_rate);
-                if flow_rate > best_flow_rate {
-                    best_flow_rate = flow_rate;
-                    best_next_valve_id = Some(valve.id);
-                    best_node_distance = Some(*distance);
-                }
-                // This is finicky and greedy. Not great but it's quick
-                if flow_rate >= 35 {
-                    break 'outer;
-                }
+            valve
+        })
+        .collect();
+
+    // Bitstring Visited creation
+    let mut valve_mask = FxHashMap::default();
+    for (idx, (key, _)) in flow_rates.iter().enumerate() {
+        valve_mask.insert(key.clone(), 1 << idx);
+    }
+
+    // Construct the Neighbors table using the IDs - Just used to build the Floyd Warshall table
+    let mut neighbors: FxHashMap<String, Vec<String>> = FxHashMap::default();
+    valves.iter().for_each(|v| {
+        neighbors.insert(v.id.clone(), v.neighbors.clone());
+    });
+
+    // Floyd Warshall Setup - Nodes that connect to other nodes get a value of 1, else a huge value
+    let mut valve_distances: FxHashMap<String, FxHashMap<String, usize>> = FxHashMap::default();
+    for (row_valve, row_neighbors) in neighbors.iter() {
+        valve_distances.insert(row_valve.clone(), FxHashMap::default());
+        for (col_valve, _) in neighbors.iter() {
+            if row_neighbors.contains(col_valve) {
+                valve_distances
+                    .get_mut(row_valve)
+                    .unwrap()
+                    .insert(col_valve.clone(), 1);
+            } else {
+                valve_distances
+                    .get_mut(row_valve)
+                    .unwrap()
+                    .insert(col_valve.clone(), 99);
             }
         }
     }
-    (best_next_valve_id, best_node_distance)
-}
 
-fn decode_valve_id(valve_id: usize) -> String {
-    let a = valve_id / 1000;
-    let b = valve_id % 100;
-    format!(
-        "{}{}",
-        char::from_u32(a as u32).unwrap(),
-        char::from_u32(b as u32).unwrap()
-    )
-        .to_string()
-}
-
-fn find_best_path(node_distances: &HashMap<(NodeIndex, NodeIndex), usize>, pipeline: &mut Pipeline) -> Vec<(usize, usize)> {
-    let mut current_valve_id = 65065_usize; // AA
-    let mut path: Vec<(usize, usize)> = Vec::new();
-
-    // Find the best path
-    for _ in 0..20 {
-        if let (Some(next_valve_id), Some(distance)) =
-        find_best_next(current_valve_id, &node_distances, pipeline)
-        {
-            let new_valve = pipeline.get_mut(&next_valve_id).unwrap();
-            current_valve_id = new_valve.id;
-            new_valve.is_open = true;
-            path.push((new_valve.id, distance));
-        }
-    }
-    path
-}
-
-/// Runs the steam release simulation for a given path and returns the total pressure released
-fn run_simulation(path: &Vec<(usize, usize)>, pipeline: &Pipeline, time_limit: usize) -> usize {
-    let mut pressure_per_tick: usize = 0;
-    let mut total_pressure_released: usize = 0;
-
-    // Run the simulation
-    let mut movement_penalty: usize = path.first().unwrap().1;
-    let mut new_flow_rate = 0;
-    let mut path_ptr: usize = 0;
-
-    for _ in 0..time_limit {
-        total_pressure_released += pressure_per_tick;
-        if movement_penalty > 0 {
-            movement_penalty = movement_penalty.saturating_sub(1);
-        } else {
-            if let Some((valve_index, _)) = path.get(path_ptr) {
-                path_ptr += 1;
-                let valve = pipeline.get(&valve_index).unwrap();
-                new_flow_rate = valve.flow_rate;
-                if let Some((_, penalty)) = path.get(path_ptr) {
-                    movement_penalty = *penalty;
-                }
-                pressure_per_tick += new_flow_rate;
+    // Tabulate Floyd Warshall table iteratively
+    for (k, _) in neighbors.iter() {
+        for (i, _) in neighbors.iter() {
+            for (j, _) in neighbors.iter() {
+                let new_value = min(
+                    *valve_distances.get(i).unwrap().get(j).unwrap(),
+                    *valve_distances.get(i).unwrap().get(k).unwrap()
+                        + *valve_distances.get(k).unwrap().get(j).unwrap(),
+                );
+                *valve_distances.get_mut(i).unwrap().get_mut(j).unwrap() = new_value;
             }
         }
     }
-    total_pressure_released
+    (valve_distances, flow_rates, valve_mask)
+}
+
+/// This is a depth-first search across the Floyd Warshall graph. However only valves that have a
+/// flow > 0 are considered as potential targets for the walk to reduce the search space. If any
+/// state of walks results in a higher flow the state is updated with the better maximum value.
+///
+/// **Key Point**
+/// The state is just a bit-mask of visited nodes. It does not contain the order that nodes were
+/// visited in. For example, the nodes AA -> DD -> BB would share state `0b1011` but so would path
+/// AA -> BB -> DD. This does not matter for this advent problem; and that is why the max value for
+/// any state needs to be updated each call against the previous max that state may have had.
+///
+fn run<'a>(
+    flow_rates: &FxHashMap<String, usize>, // Valves with flow of 0 are not included
+    valve_masks: &FxHashMap<String, usize>, // Valves with flow 0 are not included
+    valve_distances: &FxHashMap<String, FxHashMap<String, usize>>, // All valves
+    valve: String,                         // The current valve to search from
+    time_left: usize,                      // Remaining time
+    state: usize,                          // A usize of masking bits for visited valves
+    flow: usize,                           // The current flow for the state
+    answer: &'a mut FxHashMap<usize, usize>, // The best flow total for every state scanned
+) -> &'a mut FxHashMap<usize, usize> {
+    // If the flow just found is better, update our state with the new best flow
+    if answer.get(&state).is_none() {
+        answer.insert(state, 0);
+    } else {
+        let current_best = *answer.get(&state).unwrap();
+        *answer.get_mut(&state).unwrap() = max(current_best, flow);
+    }
+    // Iterate over all valves that could potentially add to the flow
+    for (next_valve, rate) in flow_rates.iter() {
+        // Move to the valve and open in, which takes an extra minute
+        let new_time = time_left
+            .saturating_sub(
+                *valve_distances
+                    .get(&valve)
+                    .unwrap()
+                    .get(next_valve)
+                    .unwrap(),
+            )
+            .saturating_sub(1);
+        if new_time == 0 {
+            continue; // Base case - Out of time!
+        }
+        if valve_masks.get(next_valve).unwrap() & state != 0 {
+            continue; // If the next valve has already been visited it is skipped
+        }
+        run(
+            flow_rates,
+            valve_masks,
+            valve_distances,
+            next_valve.clone(),
+            new_time,
+            state | valve_masks.get(next_valve).unwrap(),
+            flow + new_time * rate,
+            answer,
+        );
+    }
+    answer
 }
 
 /// Just myself with 30 minutes
 pub fn part_one(input: &str) -> Option<usize> {
-    let (graph, mut pipeline) = build_graph(input);
-    let node_distances = floyd_warshall(&graph, |edge| 1).unwrap();
-    let mut path: Vec<(usize, usize)> = find_best_path(&node_distances, &mut pipeline);
-    Some(run_simulation(&path, &pipeline, 30))
+    let (valve_distances, flow_rates, valve_masks) = build_structures(input);
+    let mut answer = FxHashMap::default();
+    run(
+        &flow_rates,
+        &valve_masks,
+        &valve_distances,
+        "AA".to_string(),
+        30,
+        0,
+        0,
+        &mut answer,
+    );
+    Some(*answer.values().max().unwrap())
 }
 
 /// The elephant and I with 26 minutes to spare
 pub fn part_two(input: &str) -> Option<usize> {
-    let (graph, mut pipeline) = build_graph(input);
-    let node_distances = floyd_warshall(&graph, |edge| 1).unwrap();
-    let mut path: Vec<(usize, usize)> = find_best_path(&node_distances, &mut pipeline);
-
-    // First we find the "best" path, then we divide and conquer
-    Some(run_simulation(&path, &pipeline, 26))
+    // This is all the same as Part 1, run a nice DFS over every possible valve state
+    let (valve_distances, flow_rates, valve_masks) = build_structures(input);
+    let mut answer = FxHashMap::default();
+    run(
+        &flow_rates,
+        &valve_masks,
+        &valve_distances,
+        "AA".to_string(),
+        26, // Gotta train that elephant up!
+        0,
+        0,
+        &mut answer,
+    );
+    // But we don't use the answer directly! Instead we take all the pairs of states that don't have
+    // any overlap and pick two that have the best sum. This is the key insight to solving this
+    // without having a "concurrent walking" strategy. Also scales to more than two players if we
+    // want it to. Thankfully we can filter "overlapping" states due to our state being fit for
+    // boolean math.
+    let mut best: usize = 0;
+    for (outer_state, outer_flow) in &answer {
+        for (inner_state, inner_flow) in &answer {
+            if outer_state & inner_state == 0 {
+                best = max(best, *outer_flow + *inner_flow);
+            }
+        }
+    }
+    Some(best)
 }
 
 fn main() {
